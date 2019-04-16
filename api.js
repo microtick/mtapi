b2772565b91e7e1b962dfbf5c1f6ab0cb1a75121
Mark ShapeShift <fumecow@gmail.com>
2019-04-16 06:50:06 -0600

    modified api and tester for api to include initial use case

--- api.js
+++ api.js
@@ -1,233 +1,151 @@
-const bip39 = require(`bip39`)
-const bip32 = require(`bip32`)
-const bech32 = require(`bech32`)
-const secp256k1 = require(`secp256k1`)
-const sha256 = require("crypto-js/sha256")
-const ripemd160 = require("crypto-js/ripemd160")
-const CryptoJS = require("crypto-js")
+const wallet = require('./wallet')
 const axios = require('axios')
 
-const hdPathAtom = `m/44'/118'/0'/0/0`
-
-const standardRandomBytesFunc = x => CryptoJS.lib.WordArray.random(x).toString()
-
-async function generateWalletFromSeed(mnemonic) {
-  const masterKey = await deriveMasterKey(mnemonic)
-  const { privateKey, publicKey } = deriveKeypair(masterKey)
-  const cosmosAddress = createCosmosAddress(publicKey)
-  return {
-    privateKey: privateKey.toString(`hex`),
-    publicKey: publicKey.toString(`hex`),
-    cosmosAddress
-  }
-}
-
-function generateSeed(randomBytesFunc = standardRandomBytesFunc) {
-  const randomBytes = Buffer.from(randomBytesFunc(32), `hex`)
-  if (randomBytes.length !== 32) throw Error(`Entropy has incorrect length`)
-  const mnemonic = bip39.entropyToMnemonic(randomBytes.toString(`hex`))
-
-  return mnemonic
-}
-
-async function generateWallet(randomBytesFunc = standardRandomBytesFunc) {
-  const mnemonic = generateSeed(randomBytesFunc)
-  return await generateWalletFromSeed(mnemonic)
-}
-
-// NOTE: this only works with a compressed public key (33 bytes)
-function createCosmosAddress(publicKey) {
-  const message = CryptoJS.enc.Hex.parse(publicKey.toString(`hex`))
-  const hash = ripemd160(sha256(message)).toString()
-  const address = Buffer.from(hash, `hex`)
-  const cosmosAddress = bech32ify(address, `cosmos`)
-
-  return cosmosAddress
-}
-
-async function deriveMasterKey(mnemonic) {
-  // throws if mnemonic is invalid
-  bip39.validateMnemonic(mnemonic)
-
-  const seed = await bip39.mnemonicToSeed(mnemonic)
-  const masterKey = await bip32.fromSeed(seed)
-  return masterKey
-}
-
-function deriveKeypair(masterKey) {
-  const cosmosHD = masterKey.derivePath(hdPathAtom)
-  const privateKey = cosmosHD.privateKey
-  const publicKey = secp256k1.publicKeyCreate(privateKey, true)
-
-  return {
-    privateKey,
-    publicKey
-  }
-}
-
-function bech32ify(address, prefix) {
-  const words = bech32.toWords(address)
-  return bech32.encode(prefix, words)
-}
-
-// Transactions often have amino decoded objects in them {type, value}.
-// We need to strip this clutter as we need to sign only the values.
-function prepareSignBytes(jsonTx) {
-  if (Array.isArray(jsonTx)) {
-    return jsonTx.map(prepareSignBytes)
-  }
-
-  // string or number
-  if (typeof jsonTx !== `object`) {
-    return jsonTx
+class API {
+  
+  constructor(tendermint, cosmos, faucet) {
+    this.tm = tendermint
+    this.cosmos = cosmos
+    this.faucet = faucet
   }
   
-  // Check for type / value keys
-  if (Object.keys(jsonTx).length === 2 && jsonTx['type'] !== undefined && jsonTx['value'] !== undefined) {
-      // remove this layer of hierarchy, process value only
-      jsonTx = jsonTx.value
+  async cosmosQuery(url) {
+    try {
+      const query = this.cosmos + url
+      console.log("Cosmos query: " + query)
+      return await axios.get(query)
+    } catch (err) {
+      throw new Error("Cosmos query failed")
+      console.log(err.stack)
+    }
   }
   
-  let sorted = {}
-  Object.keys(jsonTx)
-    .sort()
-    .forEach(key => {
-      if (jsonTx[key] === undefined || jsonTx[key] === null) return
+  async cosmosPostTx(tx) {
+    try {
+      return await axios.post(this.cosmos + "/microtick/broadcast", {
+        tx: JSON.stringify(tx)
+      })
+    } catch (err) {
+      throw new Error("Cosmos post failed")
+      console.log(err.stack)
+    }
+  }
   
-      sorted[key] = prepareSignBytes(jsonTx[key])
+  async blockInfo() {
+    try {
+      const res = await axios.get(this.tm + '/status')
+      return {
+        block: parseInt(res.data.result.sync_info.latest_block_height, 10),
+        timestamp: Math.floor(new Date(res.data.result.sync_info.latest_block_time).getTime() / 1000)
+      }
+    } catch (err) {
+      throw new Error("Block number request failed")
+    }
+  }
+  
+  async getBlock(blockNumber) {
+    try {
+      const query = this.tm + "/block?height=" + blockNumber
+      const res = await axios.get(query)
+      return res.data.result.block_meta
+    } catch (err) {
+      throw new Error("Block request failed")
+    }
+  }
+  
+  async generateWallet() {
+    this.wallet = await wallet.generate()
+    return {
+      pub: this.wallet.publicKey,
+      priv: this.wallet.privateKey,
+      acct: this.wallet.cosmosAddress
+    }
+  }
+  
+  async createAccount(keys) {
+    this.wallet = {
+      privateKey: keys.priv,
+      publicKey: keys.pub,
+      cosmosAddress: keys.acct
+    }
+    try {
+      await axios.get(this.faucet + "/" + this.wallet.cosmosAddress)
+    } catch (err) {
+      throw new Error("Unable to connect to faucet")
+    }
+  }
+  
+  async getAccountInfo(acct) {
+    const res = await this.cosmosQuery("/microtick/account/" + acct)
+    return res
+  }
+  
+  async createMarket(market) {
+    let msg = await this.cosmosQuery('/microtick/createmarket/' + this.wallet.cosmosAddress + "/" + market)
+    const unsigned = msg.data.tx
+    const signed = wallet.sign(unsigned, this.wallet, {
+      sequence: msg.data.sequence,
+      account_number: msg.data.accountNumber,
+      chain_id: msg.data.chainId
     })
-  return sorted
+    console.log(JSON.stringify(signed))
+    await this.cosmosPostTx(signed)
+  }
+  
+  async getMarketInfo(market) {
+    const res = await this.cosmosQuery("/microtick/market/" + market)
+    return res.data
+  }
+  
+  async getOrderbookInfo(market, dur) {
+    const res = await this.cosmosQuery("/microtick/orderbook/" + market + "/" + dur)
+    return res.data
+  }
+  
+  async getMarketSpot(market) {
+    const res = await this.cosmosQuery("/microtick/consensus/" + market)
+    return res.data
+  }
+  
 }
 
 /*
-The SDK expects a certain message format to serialize and then sign.
-
-type StdSignMsg struct {
-ChainID       string      `json:"chain_id"`
-AccountNumber uint64      `json:"account_number"`
-Sequence      uint64      `json:"sequence"`
-Fee           auth.StdFee `json:"fee"`
-Msgs          []sdk.Msg   `json:"msgs"`
-Memo          string      `json:"memo"`
-}
-*/
-function createSignMessage(jsonTx, sequence, account_number, chain_id) {
-  // sign bytes need amount to be an array
-  const fee = {
-    amount: jsonTx.fee.amount || [],
-    gas: jsonTx.fee.gas
-  }
-
-  return JSON.stringify(
-    prepareSignBytes({
-      fee,
-      memo: jsonTx.memo,
-      msgs: jsonTx.msg, // weird msg vs. msgs
-      sequence,
-      account_number,
-      chain_id
-    })
-  )
-}
-
-// produces the signature for a message (returns Buffer)
-function signWithPrivateKey(signMessage, privateKey) {
-  const signHash = Buffer.from(sha256(signMessage).toString(), `hex`)
-  const { signature } = secp256k1.sign(signHash, Buffer.from(privateKey, `hex`))
-  return signature
-}
-
-function createSignature(signature, publicKey) {
-  return {
-    signature: signature.toString(`base64`),
-    pub_key: {
-      type: `tendermint/PubKeySecp256k1`, // TODO: allow other keytypes
-      value: publicKey.toString(`base64`)
-    }
-  }
-}
-
-// main function to sign a jsonTx using the local keystore wallet
-// returns the complete signature object to add to the tx
-function sign(jsonTx, wallet, requestMetaData) {
-  const signMessage = createSignMessage(jsonTx, requestMetaData.sequence,
-    requestMetaData.account_number, requestMetaData.chain_id)
-  const signatureBuffer = signWithPrivateKey(signMessage, wallet.privateKey)
-  const pubKeyBuffer = Buffer.from(wallet.publicKey, `hex`)
-  return createSignature(
-    signatureBuffer,
-    pubKeyBuffer
-  )
-}
-
-// adds the signature object to the tx
-function createSignedTx(tx, signature) {
-  return Object.assign({}, tx, {
-    signatures: [signature]
-  })
-}
-
-// the broadcast body consists of the signed tx and a return type
-function createBroadcastBody(signedTx) {
-  return JSON.stringify({
-    tx: signedTx,
-    return: `block`
-  })
-}
-
 async function main() {
-  const wallet = await generateWallet()
-  const url = "microtick/createmarket/" + wallet.cosmosAddress + "/ETHUSD"
+  const localWallet = await wallet.generate()
+  console.log("Created wallet: " + localWallet.cosmosAddress)
   //const url = "microtick/createmarket/cosmos1qlzp94qve0np3du8k43epfc532rxwclxen0pnu/ETHUSD"
   try {
+    // fund account
+    console.log("Requesting funds...")
+    await axios.get('http://localhost:3000/' + localWallet.cosmosAddress)
+    
+    // generate transaction
+    const url = "microtick/createmarket/" + localWallet.cosmosAddress + "/TEST5"
     const res = await axios.get('http://localhost:1317/' + url)
     const msg = res.data
-    const signatureBuffer = signWithPrivateKey(msg, wallet.privateKey)
-    const pubKeyBuffer = Buffer.from(wallet.publicKey, `hex`)
-    const sig = createSignature(
-      signatureBuffer,
-      pubKeyBuffer
-    )
-    const signed = createSignedTx(msg, sig)
+    console.log("unsigned=" + JSON.stringify(msg))
+    
+    const signed = wallet.sign(msg.tx, localWallet, {
+      sequence: msg.sequence,
+      account_number: msg.accountNumber,
+      chain_id: msg.chainId
+    })
     console.log(JSON.stringify(signed))
+    
+    // broadcast
+    const broadurl = "microtick/broadcast"
+    const res2 = await axios.post('http://localhost:1317/' + broadurl, {
+      tx: JSON.stringify(signed)
+    }) 
+    console.log(res2.data)
   } catch (err) {
     console.log(err.message)
   }
 }
 
 main()
-
-
-/*
-async function main() {
-  var msg = {
-    type: "auth/StdTx",
-    value: {
-      msg: [
-        {
-          type: "microtick/CreateMarket",
-          value: {
-            Account: wallet.cosmosAddress,
-            Market: "LTCUSD",
-          }
-        }
-      ],
-      fee: {
-        amount: null,
-        gas: "200000"
-      },
-      signatures: null,
-      memo: ""
-    }
-  }
-  const signature = sign(msg.value, wallet, {
-      sequence: "0", 
-      account_number: "9",
-      chain_id: "mtzone"
-  })
-  msg.value = createSignedTx(msg.value, signature)
-  console.log(JSON.stringify(msg))
-}
-
-main()
 */
+
+module.exports = (tm, cosmos, faucet) => {
+  return new API(tm, cosmos, faucet)
+}
--- tester.js
+++ tester.js
@@ -1,4 +1,4 @@
-const axios = require('axios')
+//const axios = require('axios')
 
 //const url = "microtick/account/cosmos1qwu9f6zk5klej0tfs8p40j6uu8j86nh80nm3t4"
 //const url = "microtick/market/ETHUSD"
@@ -6,8 +6,9 @@ const axios = require('axios')
 //const url = "microtick/orderbook/ETHUSD/5minute"
 //const url = "microtick/quote/1"
 //const url = "microtick/trade/1"
-const url = "microtick/createmarket/cosmos1qlzp94qve0np3du8k43epfc532rxwclxen0pnu/ETHUSD"
+//const url = "microtick/createmarket/cosmos1qlzp94qve0np3du8k43epfc532rxwclxen0pnu/ETHUSD"
 
+/*
 async function main() {
     try {
         const res = await axios.get('http://localhost:1317/' + url)
@@ -16,6 +17,29 @@ async function main() {
     } catch (err) {
         console.log(err.message)
     }
+}
+
+main()
+*/
+
+const API = require('./api')
+const api = API("http://localhost:26657", "http://localhost:1317", "http://localhost:3000")
+
+async function main() {
+  //const blockInfo = await api.blockInfo()
+  //console.log(blockInfo.block)
+  //const block = await api.getBlock(blockInfo.block)
+  //console.log(block)
+  const keys = await api.generateWallet()
+  console.log(keys)
+  await api.createAccount(keys)
+  const info = await api.getAccountInfo(keys.acct)
+  console.log(info)
+  const market = "abcd1236"
+  await api.createMarket(market)
+  console.log("Market Info: " + JSON.stringify(await api.getMarketInfo(market)))
+  console.log("Orderbook Info: " + JSON.stringify(await api.getOrderbookInfo(market, "5minute")))
+  console.log("Consensus: " + JSON.stringify(await api.getMarketSpot(market)))
 }
 
 main()
